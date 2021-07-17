b749a8f50fdb2c2b2ca66a526e980e3c07f7c008
AbrarShahriar <AbrarShahriar321@gmail.com>
2021-07-17 16:07:12 +0600

    mobile auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,43 @@
 import { Email, VpnKeySharp } from '@material-ui/icons'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
 import  { useStateValue } from '../StateProvider'
 import { useHistory } from 'react-router-dom'
+import { auth } from '../firebase'
 
 function Login() {
-    const handleSubmit = e => {
-        e.preventDefault()
-        dispatch({
-            type: 'SET_USER',
-            user: 'Abrar Shahriar'
-        })
-    }
     const [{ user }, dispatch] = useStateValue()
     const history = useHistory()
     
     useEffect(() => {
         user && history.push('/')
     }, [user])
+
+    const [userCred, setUserCred] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleUserCredChange = e => {
+        setUserCred({
+            ...userCred,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = e => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        auth.signInWithEmailAndPassword(userCred.email, userCred.password)
+        .then(userData => {
+            dispatch({
+                type: 'SET_USER',
+                user: userData.user
+            })
+        })
+        .catch(err => alert(err.message))
+    }
     
     return (
         <div className="login">
@@ -29,11 +48,11 @@ function Login() {
                     <div className="form__inputs">
                         <div className="form__input">
                             <Email />
-                            <input type="email" name="email" placeholder='Email' />
+                            <input onChange={handleUserCredChange} value={userCred.email} type="email" name="email" placeholder='Email' />
                         </div>
                         <div className="form__input">
                             <VpnKeySharp />
-                            <input type="password" name="password" placeholder='Password' />
+                            <input onChange={handleUserCredChange} value={userCred.password}  type="password" name="password" placeholder='Password' />
                         </div>
 
                         <button onClick={handleSubmit}>Log in</button>
